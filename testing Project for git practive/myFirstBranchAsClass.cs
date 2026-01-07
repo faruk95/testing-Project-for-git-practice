12aa7ebfab3e0fe48a3236a7db72632cf4da7387
Faruk <faruk.ice.pust@gmail.com>
2026-01-07 17:15:58 +0600

    rest to commit 579be3a

--- testing Project for git practive/myFirstBranchAsClass.cs
+++ testing Project for git practive/myFirstBranchAsClass.cs
@@ -14,5 +14,19 @@ namespace testing_Project_for_git_practive
        string myString = "Hello World";
        string main_branch = "This is main branch"; 
     }
-   
+    public class farukBranch
+    {
+       int farukNum = 20;
+       string farukString = "Hello from Faruk Branch";
+       string faruk_branch = "This is Faruk branch";
+       // adding comment as second change 
+    }
+    public class RiponBranch
+    {
+       int riponNum = 30;
+       string riponString = "Hello from Ripon Branch";
+       string ripon_branch = "This is Ripon branch";
+       string ripon_branchs = "gg";
+    }
+
 }
