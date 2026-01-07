0c7cc1fbb88a138f345b7210ab4ebc759fb1df25
Faruk <faruk.ice.pust@gmail.com>
2026-01-07 11:34:10 +0600

    1234

---
+++ testing Project for git practive/myFirstBranchAsClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+namespace testing_Project_for_git_practive
+{
+    public class myFirstBranchAsClass
+    {
+       int myNum = 10;
+       string myString = "Hello World";
+    }
+    public class mySecondClass
+    {
+        
+    }
+}
